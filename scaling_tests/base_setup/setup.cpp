4b9b688f30f658b57676d612caeb4da82051d624
Clément Robert <cr52@protonmail.com>
2023-10-26 12:14:31 +0200

    fix setup

--- scaling_tests/base_setup/setup.cpp
+++ scaling_tests/base_setup/setup.cpp
@@ -32,8 +32,8 @@ void Setup::InitFlow(DataBlock &data) {
 
   // randomized initial positions and velocities
   for(int k = 0; k < d.PactiveCount; k++) {
-    d.Ps(PX1,k) = d.xbeg[KDIR] + idfx::randm() * (d.xend[KDIR] - d.xbeg[KDIR]);
-    d.Ps(PX2,k) = d.xbeg[KDIR] + idfx::randm() * (d.xend[KDIR] - d.xbeg[KDIR]);
+    d.Ps(PX1,k) = d.xbeg[IDIR] + idfx::randm() * (d.xend[IDIR] - d.xbeg[IDIR]);
+    d.Ps(PX2,k) = d.xbeg[JDIR] + idfx::randm() * (d.xend[JDIR] - d.xbeg[JDIR]);
     d.Ps(PX3,k) = d.xbeg[KDIR] + idfx::randm() * (d.xend[KDIR] - d.xbeg[KDIR]);
     d.Ps(PVX1,k) = 2e-3 * (0.5 - idfx::randm());
     d.Ps(PVX2,k) = 2e-3 * (0.5 - idfx::randm());
