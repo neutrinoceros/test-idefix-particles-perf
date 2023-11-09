c1fdc67aa5a082f416013e5ed6dfbfd9f98c9c30
Clément Robert <cr52@protonmail.com>
2023-11-09 16:00:12 +0100

    support plotting particle free runs

--- scaling_tests/plot.py
+++ scaling_tests/plot.py
@@ -41,19 +41,21 @@ def get_machine_label():
         warnings.warn(f"did not find {str(file)}")
 
 
-def main(argv: list[str] | None = None) -> int:
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--dir", dest="directory", required=True, type=Path)
-    args = parser.parse_args(argv)
-
+def plot_perf(
+    *,
+    axs,
+    datafiles: list[str],
+    sn_regexp: str,
+    legends: bool,
+    **kwargs,
+) -> None:
+    MARKERS = ["h", "d", "X", "^", "x"]
     sizes: list[int] = []
     nprocs: list[int] = []
     mean_perfs = []
-    datafiles = natsorted(args.directory.glob("*.json"))
+
     for datafile in datafiles:
-        if (
-            m := re.fullmatch(r"s(?P<size>\d+)_n(?P<nproc>\d+).json", datafile.name)
-        ) is not None:
+        if (m := re.fullmatch(sn_regexp, datafile.name)) is not None:
             sizes.append(int(m.group("size")))
             nprocs.append(int(m.group("nproc")))
         series = pd.read_json(datafile, typ="series")
@@ -90,6 +92,32 @@ def main(argv: list[str] | None = None) -> int:
     for k in curves.keys():
         curves[k]["perf/proc"] = curves[k]["perf"] / curves[k]["nproc"]
 
+    for i, (curve, marker) in enumerate(zip(curves.values(), MARKERS)):
+        axs[0].plot(
+            "nproc", "perf/proc", color=f"C{i}", marker=marker, data=curve, **kwargs
+        )
+
+    artists = []
+    for i, ((size, curve), marker) in enumerate(zip(curves.items(), MARKERS)):
+        (handle,) = axs[1].plot(
+            "nproc",
+            "eff",
+            color=f"C{i}",
+            marker=marker,
+            data=curve,
+            label=f"${size}^3$",
+            **kwargs,
+        )
+        artists.append(handle)
+    return artists
+
+
+def main(argv: list[str] | None = None) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--dir", dest="directory", required=True, type=Path)
+    args = parser.parse_args(argv)
+
+    # init figure
     fig, axs = plt.subplots(
         nrows=2, layout="constrained", sharex=True, gridspec_kw=dict(hspace=0, wspace=0)
     )
@@ -108,18 +136,32 @@ def main(argv: list[str] | None = None) -> int:
     )
     sax = ax.secondary_yaxis("right", functions=(lambda x: x * 8, lambda x: x / 8))
     sax.set(ylabel="Perfs [particle update/s/process]")
-    for curve in curves.values():
-        ax.plot("nproc", "perf/proc", marker="x", data=curve)
 
-    ax = axs[1]
-    ax.set(
+    axs[1].set(
         xlabel="number of processes",
         ylabel="Parallel efficiency",
     )
 
-    for size, curve in curves.items():
-        ax.plot("nproc", "eff", marker="x", data=curve, label=f"${size}^3$")
-    ax.legend(frameon=False)
+    particle_datafiles = natsorted(args.directory.glob("s*.json"))
+    wp_artists = plot_perf(
+        axs=axs,
+        datafiles=particle_datafiles,
+        sn_regexp=r"s(?P<size>\d+)_n(?P<nproc>\d+).json",
+        legends=True,
+    )
+
+    particlefree_datafiles = natsorted(args.directory.glob("NPs*.json"))
+    plot_perf(
+        axs=axs,
+        datafiles=particlefree_datafiles,
+        sn_regexp=r"NPs(?P<size>\d+)_n(?P<nproc>\d+).json",
+        legends=False,
+        linestyle="--",
+        alpha=0.6,
+    )
+
+    # finalize figure
+    axs[1].legend(handles=wp_artists, frameon=False)
 
     if label := get_machine_label():
         machine_suffix = f"_{label}"
